bc33cf1ad01070287387e1ec3c08c85c663792d0
MrGoatMan <lukenewman@codelab-18.rockhurst.int>
2020-02-26 08:05:02 -0600

    MrPlantMan

---
+++ Planet.py
@@ -0,0 +1,9 @@
+class Planet(object):
+    bodies = [Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune]
+
+    def __init__(self, suit, face):
+        self.bodyIndex = body
+
+      def __str__(self):
+        planet = Planet.bodies[self.bodyIndex]
+        return body
