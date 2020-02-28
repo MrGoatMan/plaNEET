cd31e515114ea250f9f3648138981fa414c79fbf
MrGoatMan <lukenewman@codelab-18.rockhurst.int>
2020-02-28 08:42:39 -0600

    Nitwit

--- Planetarium.py
+++ Planetarium.py
@@ -7,9 +7,4 @@ class Planetarium(object):
 
     def __str__(self):
         return '[' + ', '.join(str(planet) for planet in self.planets) + ']'
-    
-    def create(self):
-        self.planets = []
-        for i in range(4):
-            for j in range(0,4):
-                self.planets.append(Planet(i, j)
+
