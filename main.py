5cd765f28a47edec66a9f3d2dff4b160fb796a28
MrGoatMan <lukenewman@codelab-18.rockhurst.int>
2020-02-28 08:43:15 -0600

    E

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from Planetarium import Planetarium
 from Planet import Planet
 
-viewdeck = Planetarium()
-print(viewdeck)
+mercury = Planet('flat', 'brown')
+venus = Planet('gassy', 'brown')
+earth = Planet('terra', 'blue-green')
+mars = Planet('flat', 'red')
+jupiter = Planet('gassy', 'orange')
+saturn = Planet('ringed', 'brown')
+uranus = Planet('ringed', 'blue')
+neptune = Planet('flat', 'blue')
+
+print(mercury)
+print(venus)
+print(earth)
+print(mars)
+print(jupiter)
+print(saturn)
+print(uranus)
+print(neptune)
